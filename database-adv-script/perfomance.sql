72787decc7bdcad11d365faed080e9802ba43e52
Estifanos <estifanosbereket297@gmail.com>
2025-05-24 14:40:00 +0300

    Week 2 choreS

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,8 @@
 
 EXPLAIN
 SELECT * FROM bookings 
-JOIN users ON bookings.user_id=users.user_id
-JOIN properties ON bookings.property_id=properties.property_id
-LEFT JOIN payments ON bookings.booking_id=payments.booking_id;
+JOIN users ON bookings.user_id = users.user_id
+JOIN properties ON bookings.property_id = properties.property_id
+LEFT JOIN payments ON bookings.booking_id = payments.booking_id
+WHERE bookings.status = 'confirmed'
+  AND bookings.start_date > '2024-01-01';
